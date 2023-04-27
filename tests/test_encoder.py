2d60acf23ed5a5407894da0a1194e32251b38e98
makarkarius <makark22@gmail.com>
2023-04-27 23:21:12 +0300

    fix: tests

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -1,13 +1,11 @@
 from business.encoder import encode_acts_diagram, encode_class_diagram
 from core.model import Class, Field, Acts
 
-str = "@startuml\nclass ааа {\n+ 123 asdas\n}\n@enduml"
-
 
 def test_cd_encoder():
     cl = [Class('ааа', 'class')]
     cl[0].flds.append(Field('asdas', '123', 'public'))
-    assert encode_class_diagram(cl, []) == str
+    assert encode_class_diagram(cl, []) == "@startuml\nclass ааа {\n+ 123 asdas\n}\n@enduml"
 
 
 def test_ac_encoder():
